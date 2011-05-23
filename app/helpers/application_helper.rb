8f5ebc56ae6724afa79c3c725a8ce912732bba4b
Sue Grimes <sgrimes@stanford.edu>
2011-05-23 14:29:16 -0700

    javascript changes - move superfish.js to public/files - rename toggle_div helper to div_toggle to avoid js conflict

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,10 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
-  def toggle_div(div)
-    update_page { |page| page[div].toggle }
+  def div_toggle(div, div1=nil)
+    update_page do |page|
+      page[div].toggle
+      page[div1].toggle if !div1.nil?
+    end      
   end
   
   def version_name(version_id)
