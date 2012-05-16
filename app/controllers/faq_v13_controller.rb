9e020cfaa664f3e70a3b973b7dbd5b08f202a319
Sue Grimes <sgrimes@stanford.edu>
2012-05-16 15:53:22 -0700

    Work in progress - generalize for multiple exome design versions

--- app/controllers/faq_v13_controller.rb
+++ app/controllers/faq_v13_controller.rb
@@ -9,16 +9,6 @@ class FaqV13Controller < ApplicationController
    #file_path = File.join(RAILS_ROOT, "public/images", "Figures_S1_Natsoulis.png")
    #send_file file_path, :type => 'image/png', :disposition => 'inline'
   end
- 
-  def table_s5
-    @table1 = read_table(File.join(FILE_PATH, "Table_S5_Novels_SNVs.txt"))
-    #send_file(File.join(FILE_PATH, "Table_S5_Novels_SNVs.txt"), :type => 'text/csv', :disposition => 'inline')
-  end
-  
-  def table_s8
-    #@table1 = read_table(File.join(FILE_PATH, "Table_S8_Capture_Oligos.txt"))
-    send_file(File.join(FILE_PATH, "Table_S8_Capture_Oligos.txt"), :type => 'text/csv', :disposition => 'inline')
-  end
 
   def statistics
     @table1 = read_table(File.join(FILE_PATH, "oligostats_table2.txt"))
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,7 @@ class WelcomeController < ApplicationController
   skip_before_filter :login_required, :only => :welcome
 
   def welcome
+    render :action => "v#{EXOME_VERSION}_welcome"
   end
   
   def notimplemented
